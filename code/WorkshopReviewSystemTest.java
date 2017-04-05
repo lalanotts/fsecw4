72a0d1c44f3140b7454967676a9293c034edf824
Luou Wen <luouwen97@gmail.com>
2017-04-05 15:40:53 +0100

    Luou - Updated PaperOverview Test

--- code/WorkshopReviewSystemTest.java
+++ code/WorkshopReviewSystemTest.java
@@ -1,27 +1,61 @@
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 import org.junit.Test;
+
+import org.junit.Before;
+import org.junit.After;
 
 public class WorkshopReviewSystemTest {
 	
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	private ByteArrayInputStream inContent;
 	
-	ArrayList<WorkshopPaper> AllPapers = new ArrayList<WorkshopPaper>();
+	@Before
+	public void setUpStreams(){
+		System.setOut(new PrintStream(outContent));
+	}
 	
+	//Date Created: 05-04-17; Authors: Luou WEN, Yanting SHEN
+	//Failed - check association - 31/3/17 21:00
 	@Test
-	public void testPrintPaperOverviewwithPapers() {
+	public void testPrintPaperOverviewWithPapers() {
+		WorkshopReviewSystem.AllPapers = new ArrayList<WorkshopPaper>();
 		
-		fail("Not yet implemented");
+		WorkshopPaper p1 = new WorkshopPaper("Paper 1 is great");
+		p1.addReview(new WorkshopReview(4,"This paper is pretty good."));
+		p1.addReview(new WorkshopReview(3,"This paper is good for the workshop."));
+		p1.addReview(new WorkshopReview(2, "This paper is pretty mediocre."));
+		
+		WorkshopReviewSystem.AllPapers.add(p1);
+		
+		WorkshopPaper p2 = new WorkshopPaper("Paper 2 is my best work");
+		p2.addReview(new WorkshopReview(2,"This can hardly be his best work"));
+		p2.addReview(new WorkshopReview(1,"Ive read better articles in Hello Magazine"));
+		p2.addReview(new WorkshopReview(1,"So painful to read."));
+		
+		WorkshopReviewSystem.AllPapers.add(p2);
+		
+		WorkshopReviewSystem.PrintPaperOverview();
+		
+		String expectedOut = "1) Paper 1 is great - 3.0\r\n";
+		expectedOut += "2) Paper 2 is my best work - 1.3333334\r\n";
+		
+		assertEquals(expectedOut, outContent.toString());
 	}
 	
 	@Test
 	public void testPrintPaperOverviewNoPaper() {
 		fail("Not yet implemented");
+	}
+	
+	@After
+	public void cleanUp(){
+		//clean up Streams
+		System.setOut(System.out);
+		System.setIn(System.in);
 	}
 
 }
