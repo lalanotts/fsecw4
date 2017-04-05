9fd602b9907ac6c02138a4cb329bb9f19df66f3b
Luou Wen <luouwen97@gmail.com>
2017-04-05 12:13:03 +0100

    Luou -- Added base variables.

--- code/WorkshopReviewSystemTest.java
+++ code/WorkshopReviewSystemTest.java
@@ -1,11 +1,26 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class WorkshopReviewSystemTest {
-
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private ByteArrayInputStream inContent;
+	
+	ArrayList<WorkshopPaper> AllPapers = new ArrayList<WorkshopPaper>();
+	
 	@Test
-	public void test() {
+	public void testPrintPaperOverviewwithPapers() {
+		
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPrintPaperOverviewNoPaper() {
 		fail("Not yet implemented");
 	}
 
