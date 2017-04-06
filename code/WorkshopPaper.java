c6ffccc416c2ae99026781f0d856565c59b95ff7
lalanotts <lalanotts@outlook.com>
2017-04-06 14:56:07 +0100

    change

--- code/WorkshopPaper.java
+++ code/WorkshopPaper.java
@@ -3,7 +3,7 @@ public class WorkshopPaper {
 	//Changed all variables from private to public for testing -- WEN & SHEN
 	public String PTitle;
 	public WorkshopReview[] PReviews;
-	public static String[] ROutputs = new String[]{"*","**","***","****","*****"};
+	public static String[] ROutputs = new String[]{"","*","**","***","****","*****"};
 
 	public WorkshopPaper() {
 		PTitle = "New Paper";
@@ -35,20 +35,25 @@ public class WorkshopPaper {
 			throw new Exception();
 	}
 	
-	public void addReview(WorkshopReview nReview) {
-		if (PReviews[0] == null)
-			PReviews[0] = nReview;
-		else {
-			if (PReviews[1] == null)
-				PReviews[1] = nReview;
+	public void addReview(WorkshopReview nReview) throws Exception{
+		if(nReview != null){
+			if (PReviews[0] == null)
+				PReviews[0] = nReview;
 			else {
-				if (PReviews[2] == null)
-					PReviews[2] = nReview;
+				if (PReviews[1] == null)
+					PReviews[1] = nReview;
+				else {
+					if (PReviews[2] == null)
+						PReviews[2] = nReview;
+					else 
+						throw new Exception();
+				}
 			}
-		}
+		}else
+			throw new Exception();		
 	}
 	
-	public float getAverageScore(){
+	public float getAverageScore() throws Exception{
 		float AvgScore = 0;
 		int numReviews = 0;
 		if (PReviews[0] != null){
@@ -63,13 +68,18 @@ public class WorkshopPaper {
 			AvgScore += PReviews[2].getRScore();
 			numReviews++;
 		}
+		if (numReviews == 0){
+			throw new Exception();
+		}
 		AvgScore = AvgScore/numReviews; //what if numReviews = 0 - 31/3/17
 		return AvgScore;
 	}
 	
 	public String toString(){
 		String myoutput = "";
-		myoutput = "Average Score = " + ROutputs[Math.round(getAverageScore())] + "\n\n"; //ROutputs index[0~4] Math.round.out[1~5] - 31/3/17
+		try {
+			myoutput = "Average Score = " + ROutputs[Math.round(getAverageScore())] + "\n\n";
+		} catch (Exception e) {} //ROutputs index[0~4] Math.round.out[1~5] - 31/3/17
 		myoutput += "Review 1:\n" + PReviews[0].toString() + "\n";
 		myoutput += "Review 2:\n" + PReviews[1].toString() + "\n";
 		myoutput += "Review 3:\n" + PReviews[2].toString() + "\n";
